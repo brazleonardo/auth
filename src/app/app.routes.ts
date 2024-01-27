2009be2cbae63bbfb3d034eae4691cb2af8255cd
Braz Leonardo Araujo <brazleonardo@MacBook-Air-de-Braz.local>
2024-01-27 17:48:49 -0300

    Primeiro commit auth

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { authGuard } from './guards/auth.guard';
+import { Page404Component } from './pages/page404/page404.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    canActivate: [authGuard]
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: '**',
+    component: Page404Component
+  },
+];
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  appName: 'auth',
+  apiUrl: 'http://localhost/api'
+};
