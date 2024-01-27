cfbf95d9bbac47fe551f8de9e6e19fbba4bb2b38
Braz Leonardo Araujo <brazleonardo@MacBook-Air-de-Braz.local>
2024-01-27 19:46:02 -0300

    Ajuste navabar

--- src/app/components/layout/layout.component.ts
+++ src/app/components/layout/layout.component.ts
@@ -2,12 +2,21 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [RouterOutlet, MatToolbarModule, MatButtonModule, MatIconModule],
+  imports: [
+    RouterOutlet,
+    MatToolbarModule,
+    MatButtonModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatIconModule
+  ],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss'
 })
