6396fc980de5b33d9e3b5c61968f09e22222b846
Braz Leonardo Araujo <brazleonardo@MacBook-Air-de-Braz.local>
2024-01-28 12:13:51 -0300

    Ajuste estrutura layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
-import { LayoutComponent } from '@@components/layout/layout.component';
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [LayoutComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
 })
 export class AppComponent {
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,5 @@
 import { Routes } from '@angular/router';
+import { LayoutComponent } from '@@components/layout/layout.component';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
 import { authGuard } from './guards/auth.guard';
@@ -7,12 +8,23 @@ import { Page404Component } from './pages/page404/page404.component';
 export const routes: Routes = [
   {
     path: '',
-    component: HomeComponent,
-    canActivate: [authGuard]
+    redirectTo: 'login',
+    pathMatch: 'full'
   },
   {
     path: 'login',
     component: LoginComponent,
+  },
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      {
+        path: 'home',
+        component: HomeComponent,
+        canActivate: [authGuard],
+      }
+    ]
   },
   {
     path: '**',
