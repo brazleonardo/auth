765020a385fe38d16562b00f907e7a5b5d832b9e
Braz Leonardo Araujo <brazleonardo@MacBook-Air-de-Braz.local>
2024-01-28 13:48:37 -0300

    Ajuste tela de login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
 import { LayoutComponent } from '@@components/layout/layout.component';
+import { LayoutAuthComponent } from '@@components/layout-auth/layout-auth.component';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
 import { authGuard } from './guards/auth.guard';
@@ -8,12 +9,18 @@ import { Page404Component } from './pages/page404/page404.component';
 export const routes: Routes = [
   {
     path: '',
-    redirectTo: 'login',
+    redirectTo: 'auth/login',
     pathMatch: 'full'
   },
   {
-    path: 'login',
-    component: LoginComponent,
+    path: 'auth',
+    component: LayoutAuthComponent,
+    children: [
+      {
+        path: 'login',
+        component: LoginComponent
+      }
+    ]
   },
   {
     path: '',
---
+++ src/app/components/layout-auth/layout-auth.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import {MatGridListModule} from '@angular/material/grid-list';
+
+
+@Component({
+  selector: 'app-layout-auth',
+  standalone: true,
+  imports: [RouterOutlet, MatGridListModule],
+  templateUrl: './layout-auth.component.html',
+  styleUrl: './layout-auth.component.scss'
+})
+export class LayoutAuthComponent {
+
+}
