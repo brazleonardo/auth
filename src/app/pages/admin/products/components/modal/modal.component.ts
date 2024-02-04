d4628ec0404eb3afd81b4f7c379f25a2169eedaf
Braz Leonardo Araujo <brazleonardo@MacBook-Air-de-Braz.local>
2024-02-03 23:08:19 -0300

    Ajuste modal detalhes do produto

--- src/app/pages/admin/products/components/modal/modal.component.ts
+++ src/app/pages/admin/products/components/modal/modal.component.ts
@@ -1,8 +1,11 @@
-import { Component, Inject } from '@angular/core';
+import { Component, Inject, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { MAT_DIALOG_DATA, MatDialogModule, MatDialogTitle } from '@angular/material/dialog';
 import { ReactiveFormsModule, FormsModule, FormGroup, FormControl, Validators } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatGridListModule } from '@angular/material/grid-list';
 import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
 import { MatButtonModule } from '@angular/material/button';
 
 export interface DialogData {
@@ -16,14 +19,38 @@ export interface DialogData {
     MatDialogModule,
     MatDialogTitle,
     ReactiveFormsModule,
+    CommonModule,
     FormsModule,
     MatFormFieldModule,
+    MatGridListModule,
     MatInputModule,
+    MatSelectModule,
     MatButtonModule,
   ],
   templateUrl: './modal.component.html',
   styleUrl: './modal.component.scss'
 })
 export class ModalComponent {
-  constructor(@Inject(MAT_DIALOG_DATA) public data: DialogData) {}
+  isLoading = signal(false);
+  protected productForm!: FormGroup;
+
+  constructor(@Inject(MAT_DIALOG_DATA) public data: DialogData) {
+    this.productForm = new FormGroup({
+      title: new FormControl('', [Validators.required]),
+      description: new FormControl('', [Validators.required]),
+      brand: new FormControl('', [Validators.required]),
+      category: new FormControl('', [Validators.required]),
+      price: new FormControl('', [Validators.required]),
+      discountPercentage: new FormControl('', [Validators.required]),
+      rating: new FormControl('', [Validators.required]),
+      stock: new FormControl('', [Validators.required]),
+    })
+  }
+
+  onSubmit(){
+    if(this.productForm.valid){
+      this.isLoading.set(true);
+      console.log(this.productForm.value);
+    }
+  }
 }
