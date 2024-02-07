b80aa2c27acb9368d3482e30c0b509848091f244
Leonardo Araujo <leonardo.araujo@qualityinteligencia.com.br>
2024-02-07 16:29:18 -0300

    Add página de listagem de categorias

--- src/app/pages/admin/categories/categories.component.ts
+++ src/app/pages/admin/categories/categories.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, inject, } from '@angular/core';
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 
+import { AuthService } from '@@services/auth.service';
 import { CategoryService } from '@@services/category.service';
 
 @Component({
@@ -12,11 +13,17 @@ import { CategoryService } from '@@services/category.service';
 export default class CategoriesComponent implements OnInit {
   displayedColumns: string[] = ['pos', 'name'];
   dataSource = new MatTableDataSource<string>([]);
+  protected authService = inject(AuthService);
   protected categoryService = inject(CategoryService);
 
 
   ngOnInit(): void {
+    this.getUser();
     this.getCategories();
+  }
+
+  getUser(){
+    this.authService.me().subscribe();
   }
 
   getCategories(){
