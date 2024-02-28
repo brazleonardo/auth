153ed384ad02946f75ba513bfd2a770bdc2ae2cd
Leonardo Araujo <leonardo.araujo@qualityinteligencia.com.br>
2024-02-28 11:08:46 -0300

    Ajuste search página de categorias

--- src/app/pages/admin/categories/categories.component.ts
+++ src/app/pages/admin/categories/categories.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit, OnDestroy, inject } from '@angular/core'
+import { AsyncPipe } from '@angular/common'
+import { ActivatedRoute } from '@angular/router'
+import { map } from 'rxjs'
 import { MatTableDataSource, MatTableModule } from '@angular/material/table'
 
 import { FilterAdminService } from '@@services/filter-admin.service'
@@ -7,17 +10,37 @@ import { CategoryService } from '@@services/category.service'
 
 @Component({
   standalone: true,
-  imports: [MatTableModule],
+  imports: [AsyncPipe, MatTableModule],
   templateUrl: './categories.component.html',
   styleUrl: './categories.component.scss',
 })
 export default class CategoriesComponent implements OnInit, OnDestroy {
-  protected displayedColumns: string[] = ['pos', 'name']
-  protected dataSource = new MatTableDataSource<string>([])
-
   private filterAdminService = inject(FilterAdminService)
   private authService = inject(AuthService)
   private categoryService = inject(CategoryService)
+  private activatedRoute = inject(ActivatedRoute)
+
+  protected displayedColumns: string[] = ['pos', 'name']
+  protected dataSource = new MatTableDataSource<string>([])
+
+  protected categories$ = this.activatedRoute.queryParamMap.pipe(
+    map((value) => {
+      const search = value.get('search') ?? ''
+
+      return this.categoryService.categories().subscribe({
+        next: (response) => {
+          if (search) {
+            const searchResponse = response.filter(
+              (item) => item.toLowerCase().indexOf(search.toLowerCase()) >= 0,
+            )
+            this.dataSource = new MatTableDataSource<string>(searchResponse)
+            return
+          }
+          this.dataSource = new MatTableDataSource<string>(response)
+        },
+      })
+    }),
+  )
 
   ngOnInit(): void {
     this.filterAdminService.setHasFilter = {
@@ -25,7 +48,6 @@ export default class CategoriesComponent implements OnInit, OnDestroy {
       options: false,
     }
     this.getUser()
-    this.getCategories()
   }
 
   getUser() {
