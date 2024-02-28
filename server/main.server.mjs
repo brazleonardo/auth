435a2fdecf28e9462f2a53ccc172fa1ee304c719
Leonardo Araujo <leonardo.araujo@qualityinteligencia.com.br>
2024-02-28 16:21:59 -0300

    Auto-generated commit

---
+++ server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-AHY2LABA.mjs";import"./chunk-RE77IPRV.mjs";import"./chunk-EGO2J43F.mjs";import"./chunk-HDRQP727.mjs";import"./chunk-TWRRTGSF.mjs";import"./chunk-JAQV76DB.mjs";import"./chunk-UTEJNEE6.mjs";import"./chunk-QDO4PRKN.mjs";import"./chunk-LFZKNOPU.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
